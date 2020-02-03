65b31f4ec0794f49c84ff441ae80f95a420e755a
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-03 05:16:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ def self.all_students_in_grade_9
   arr = DB[:conn].execute(sql)
   arr
 end
-def self.all_students_below_12th_grade
+def self.students_below_12th_grade
  arr = []
   sql = <<-SQL
   SELECT * FROM students WHERE grade < 12;
